118b2a3fbc7650a7bd972917a5a17b36c5f67e0b
鄢桀蚀荼-YanjerTS <yanjerts@outlook.com>
2025-12-28 10:57:23 +0800

    2.0版本新页面

--- src/js/sonPageJump.js
+++ src/js/sonPageJump.js
@@ -1,6 +1,21 @@
+// const pzs = develop
+const pzs = github
+
+githubLocation = window.location.href
+
+
 document.getElementById('article-icon').addEventListener('click', function() {
-    window.location.href = '../../page/article.html';
+    if(pzs === 'develop'){
+        window.location.href = '../../page/article.html'
+    }else{
+        window.location.href = githubLocation + '/page/article.html'
+    }
+
 });
 document.getElementById('backup').addEventListener('click', function() {
-    window.location.href = '../../-ApoxtleBreaker/index.html';
+    if(pzs === 'develop'){
+        window.location.href = '../../-ApoxtleBreaker/index.html';
+    }else{
+        window.location.href = githubLocation + '/-ApoxtleBreaker/index.html';
+    }
 });
